69545ad6050d8a1d35a8f52d47fc586b979e58cb
Coed66 <tuanbkak66@gmail.com>
2023-12-17 20:14:14 +0700

    Launch Program

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import controller.ProgramController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Program extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        final String PROGRAM_PATH = "/View/Program2.fxml";
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(PROGRAM_PATH));
+        ProgramController viewStoreController = new ProgramController();
+        fxmlLoader.setController(viewStoreController);
+        Parent root = fxmlLoader.load();
+
+        stage.setTitle("Home Page");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
---
+++ src/controller/ProgramController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
+
+import java.io.IOException;
+
+public class ProgramController {
+    @FXML
+    private GridPane recentTweet;
+
+    public void initialize() throws IOException {
+        final String FILE_PATH = "/View/Tweet.fxml";
+        int column=0, row=1;
+        for(int i=0;i<1;i++){
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource(FILE_PATH));
+            PostController postController = new PostController();
+            fxmlLoader.setController(postController);
+            AnchorPane anchorPane = new AnchorPane();
+            anchorPane=fxmlLoader.load();
+            postController.setData("Le Tuan", "hello", "12", "12");
+
+
+            // VERY IMPORTANT HERE
+            recentTweet.add(anchorPane, column,row++);
+
+            // Insets: top right bottom left
+            GridPane.setMargin(anchorPane,new Insets(0,10,10,5));
+    //        recentTweet.add(fxmlLoader.load(), column,row++);
+        }
+    }
+}
