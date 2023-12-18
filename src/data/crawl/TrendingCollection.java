ae012616e28db30c272bbfb02b334c97bfa13c0b
Duong Gioi <118875482+DGioi@users.noreply.github.com>
2023-12-19 01:11:46 +0700

    update data

---
+++ src/data/crawl/TrendingCollection.java
@@ -0,0 +1,131 @@
+package data.crawl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+
+public class TrendingCollection{
+	static String url = "https://www.okx.com/vi/web3/marketplace/rankings";
+	final static String[] LABELS ={"Collection","Volume","FloorPrice","Liquidity","listed","base"};
+	final static String PATH_TO_JSON ="D:\\a_learning_code\\java\\ProjectOOPLT\\data\\json\\collectiontrending.json";
+	final static String PATH_TO_WEBDRIVER = "C:\\Users\\Admin\\Downloads\\Compressed\\chromedriver-win64\\chromedriver.exe";
+	
+	public static void runTestSelenium() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",PATH_TO_WEBDRIVER);
+        WebDriver driver = new ChromeDriver();
+        List<String> collections = new ArrayList<String>();
+        List<String> volumes = new ArrayList<String>();
+        List<String> floorPrices = new ArrayList<String>();
+        List<String> liquiditys = new ArrayList<String>();
+        List<String> listeds = new ArrayList<String>();
+        List<String> bases = new ArrayList<String>();
+        
+        
+		for(int i = 1 ; i <= 21; i++) {
+			String page = String.format("%s/page/%d", url,i);
+			 driver.get(page);
+			 
+			 JavascriptExecutor js = (JavascriptExecutor) driver;
+			 int j = 0;
+			 while(true) {
+				 j++;
+				 if(j>10) break;
+				 try {
+					driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[3]/div[3]/div/div[1]/div/a[100]"));
+					break;
+				} catch (Exception e) {
+					js.executeScript("window.scrollBy(0, 400)");
+					// TODO: handle exception
+				}
+			 }
+			 
+			 List<WebElement> webElements = driver.findElements(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[3]/div[3]/div/div[1]/div/a/div"));
+			 for (WebElement webElement : webElements) {
+				String collection = webElement.findElement(By.xpath(".//div[2]/div/div[2]/div/div[1]/span")).getText();
+				System.out.println(collection);
+				String volume = webElement.findElement(By.xpath(".//div[3]/div/span")).getText();
+//				System.out.println(volume);
+				String floorPrice = webElement.findElement(By.xpath(".//div[5]")).getText();
+//				System.out.println(floorPrice);
+				String liquidity = webElement.findElement(By.xpath(".//div[6]/div")).getText();
+//				System.out.println(liquidity);
+				String listed = webElement.findElement(By.xpath(".//div[7]")).getText();
+//				System.out.println(listed);
+				String base = webElement.findElement(By.xpath(".//div[3]/img")).getAttribute("alt");
+//				System.out.println(base);
+				collections.add(collection);
+				volumes.add(volume);
+				floorPrices.add(floorPrice);
+				liquiditys.add(liquidity);
+				listeds.add(listed);
+				bases.add(base);
+			}
+			 
+		}
+		
+		
+        
+		// Đóng trình duyệt
+        driver.quit();
+        try {
+          
+
+            // Thêm dữ liệu vào DataFrame (điều này phải được điều chỉnh tùy thuộc vào cách bạn tổ chức dữ liệu)
+            List<List<String>> dataFrame = new ArrayList<>();
+            dataFrame.add(collections);
+            dataFrame.add(volumes);
+            dataFrame.add(floorPrices);
+            dataFrame.add(liquiditys);
+            dataFrame.add(listeds);
+            dataFrame.add(bases);
+        
+            
+            // Tạo dữ liệu từ DataFrame
+            List<Map<String,String>> jsonData = createJsonData(dataFrame);
+
+            // Xuất dữ liệu ra file JSON
+            writeJsonFile(jsonData, PATH_TO_JSON);
+            System.out.println("Dữ liệu đã được xuất ra file JSON thành công.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
+
+	    private static List<Map<String, String>> createJsonData(List<List<String>> dataFrame) {
+	        List<Map<String, String>> jsonData = new ArrayList<>();
+	
+	        for (int i = 0; i < dataFrame.get(0).size(); i++) {
+	            Map<String, String> record = new HashMap<>();
+	            for (int j = 0; j < dataFrame.size(); j++) {
+	                record.put(LABELS[j], dataFrame.get(j).get(i));
+	            }
+	            jsonData.add(record);
+	        }
+	
+	        return jsonData;
+	    }
+	
+	    private static void writeJsonFile(List<Map<String,String>> jsonData, String jsonFilePath) throws IOException {
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+	        objectMapper.writeValue(new File(jsonFilePath), jsonData);
+	    }
+
+    public static void main(String[] args) throws InterruptedException {
+		runTestSelenium();
+	}
+}
