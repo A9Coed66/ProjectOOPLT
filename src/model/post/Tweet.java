d9ad5a380fb4d16af4316a875b84aa465cef2ed6
PhanTrungDuc <phantrungduc2522005@gmail.com>
2023-12-19 19:23:02 +0700

    update

--- src/model/post/Tweet.java
+++ src/model/post/Tweet.java
@@ -1,6 +1,6 @@
 package model.post;
 
-public class TwitterPost extends Post{
+public class Tweet extends Post{
 
 //	**
 //	Attribute
@@ -20,10 +20,10 @@ public class TwitterPost extends Post{
 //	**
 //	Constructor
 //	**
-	public TwitterPost(String content, String imageUrl, String author, String dateCreated, String url, String reply, String like, String avatarUrl) {
+	public Tweet(String content, String imageUrl, String author, String dateCreated, String url, String reply, String like, String avatarUrl) {
 		super(content, imageUrl, author, dateCreated, url, reply, like, avatarUrl);
 	}
-	public TwitterPost(String content, String imageUrl, String author, String dateCreated, String url, String reply, String like, String avatarUrl, String hashtag, String retweet) {
+	public Tweet(String content, String imageUrl, String author, String dateCreated, String url, String reply, String like, String avatarUrl, String hashtag, String retweet) {
 		super(content, imageUrl, author, dateCreated, url, reply, like, avatarUrl);
 		this.hashtag = hashtag;
 		this.retweet = retweet;
