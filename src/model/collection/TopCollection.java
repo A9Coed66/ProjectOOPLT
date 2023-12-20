7533f32fefc60be9b16641d21eaf39e24c539fcf
Coed66 <tuanbkak66@gmail.com>
2023-12-20 12:58:01 +0700

    Not write interface so I delete

--- src/model/collection/TopCollection.java
+++ src/model/collection/TopCollection.java
@@ -1,7 +1,7 @@
 package model.collection;
-import data.crawl.IDataFrame;
+//import data.crawl.IDataFrame;
 
-public class TopCollection implements IDataFrame{
+public class TopCollection{
 	private int top;
 	private String name ;
 	private String volume ;
