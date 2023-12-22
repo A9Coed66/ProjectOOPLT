a117eef5aa2a28e67e7ccf8910eac7e072197be9
Coed66 <tuanbkak66@gmail.com>
2023-12-22 16:08:48 +0700

    Test new type of DataPoint in new independence class

---
+++ src/analyze/DataPointv1.java
@@ -0,0 +1,28 @@
+package analyze;
+
+public class DataPointv1 {
+    // Collection data point properties: name, nftRanking, number tweet blog, ranking tweet blog, volume in a day
+    private String name;
+    private int nftRanking;
+    private int volume; // Co the update thanh list luu volume trong 1-7-30 ngay
+    private int numberTweetblog;
+    private int tweetblogRanking;
+    public String getName() {
+        return name;
+    }
+
+    public int getNftRanking() {
+        return nftRanking;
+    }
+
+    public int getTweetBlogRanking() {
+        return tweetblogRanking;
+    }
+
+    public DataPointv1(String name, int nftRanking, int tweetblogRanking) {
+        this.name = name;
+        this.nftRanking = nftRanking;
+        this.tweetblogRanking = tweetblogRanking;
+    }
+}
+
