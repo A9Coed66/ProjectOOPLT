db7bb0b9dc8502a05dd220a336b0ce97d9f5e8eb
Coed66 <tuanbkak66@gmail.com>
2023-12-22 16:09:42 +0700

    new class is responsible for helping us find the top 10 trending collections

---
+++ src/controller/algorithm/FindTop10Collection.java
@@ -0,0 +1,6 @@
+package controller.algorithm;
+
+public class FindTop10Collection {
+    private final String TOP_COLLECTION_FILE_PATH = "/data/json/collection/okx/topcollection_20231221_160726.json";
+
+}
